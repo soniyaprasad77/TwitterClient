dc2af6e85d6c4d194cf1f1a3777ff2821c96d0c8
Your Name <soniyagupta82270@gmail.com>
2024-04-21 17:43:37 +0530

    Done with UI

--- Client/pages/index.tsx
+++ Client/pages/index.tsx
@@ -44,6 +44,10 @@ const sideMenuItems: xSidebarButtons[] = [
     icon: <LiaClipboardListSolid />,
   },
   {
+    title: "Premium",
+    icon: <BsTwitterX />,
+  },
+  {
     title: "Profile",
     icon: <FaUserAlt />,
   },
@@ -56,29 +60,51 @@ const sideMenuItems: xSidebarButtons[] = [
 export default function Home() {
   return (
     <div className={inter.className}>
-    <div className="grid grid-cols-12 h-screen w-screen pl-40">
-      <div className="col-span-2 pt-1">
-        <div className="text-3xl h-fit hover:bg-slate-800 rounded-full px-3 py-2  w-fit">
-          <BsTwitterX />
+      <div className="grid grid-cols-12 h-screen w-screen pl-40">
+        <div className="col-span-2 pt-1">
+          <div className="text-3xl h-fit hover:bg-slate-800 rounded-full px-3 py-2  w-fit">
+            <BsTwitterX />
+          </div>
+          <div className="mt-4 text-xl font-bold">
+            <ul>
+              {sideMenuItems.map((item) => (
+                <li
+                  key={item.title}
+                  className="flex flex-start items-center gap-5 hover:bg-slate-800 rounded-full w-fit px-2 py-3 cursor-pointer"
+                >
+                  <span className="text-3xl">{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <button className="bg-[#1d9bf0] w-full p-3 rounded-full mx-[-10px] my-[20px] text-lg">
+              Post
+            </button>
+          </div>
         </div>
-        <div className="mt-4 text-xl font-bold">
-          <ul>
-            {sideMenuItems.map((item) => (
-              <li key={item.title}
-              className="flex flex-start items-center gap-5 hover:bg-slate-800 rounded-full w-fit px-2 py-3 cursor-pointer">
-                 <span className="text-3xl">{item.icon}</span><span>{item.title}</span>
-              </li>
-            ))}
-          </ul>
-          <button className="bg-[#1d9bf0] w-full p-3 rounded-full mx-[-10px] my-[20px] text-lg">Post</button>
+        <div className="col-span-6  border-r-[1px] border-l-[1px] border-gray-600 h-screen overflow-scroll hide-scrollbar cursor-pointer transition-all">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
-      </div>
-      <div className="col-span-6 border border-r-[1px] border-l-[1px] border-gray-600 cursor-pointer transition-all">
-        <FeedCard/>
-      </div>
 
-      <div className="col-span-3"></div>
-    </div>
+        <div className="col-span-3"></div>
+      </div>
     </div>
   );
 }
--- Client/tailwind.config.ts
+++ Client/tailwind.config.ts
@@ -13,8 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      scrollbar: ["rounded"],
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar')
+  ],
 };
 export default config;
