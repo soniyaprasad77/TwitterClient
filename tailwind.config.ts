5b73ecccf9cbd64def0bc235ee0c034a46f08bbd
Your Name <soniyagupta82270@gmail.com>
2024-05-02 02:20:38 +0530

    minor fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,19 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      scrollbar: ["rounded"],
     },
   },
-  plugins: [
-    require('tailwind-scrollbar')
-  ],
-};
-export default config;
+  plugins: [],
+}
